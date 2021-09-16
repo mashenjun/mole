f56d8f1b949205a3e70272b60562e26ac6215967
Shenjun <mashenjun0902@gmail.com>
2021-09-16 16:21:51 +0800

    add qps metrics
    Signed-off-by: Shenjun <mashenjun0902@gmail.com>

--- data-analysis/prom_metrics_feature_basic.py
+++ data-analysis/prom_metrics_feature_basic.py
@@ -4,16 +4,17 @@ import pandas as pd
 import argparse
 from tsfresh.feature_extraction import extract_features, MinimalFCParameters
 
+# cnt is the positive value count
 feature_cols = ['metrics', 'sum', 'median', 'mean',
                 'length', 'standard_deviation',
-                'variance', 'maximum', 'minimum', 'mean_mean', 'maximum_mean']
+                'variance', 'maximum', 'minimum', 'mean_mean', 'maximum_mean', 'sum_div_cnt', 'mean_sum_div_cnt', 'maximum_sum_div_cnt']
 
 
 def load_csv(file_name: str):
     return pd.read_csv(file_name, dtype='float')
 
 
-# return a df contain feature
+# return a df contain all basic feature
 def extract_feature(df: pd.DataFrame, metrics_name: str, need_summary: bool):
     table = pd.DataFrame(columns=feature_cols)
     cols = df.columns[1:]
@@ -24,18 +25,31 @@ def extract_feature(df: pd.DataFrame, metrics_name: str, need_summary: bool):
         # print(ddf.head())
         extracted_features = extract_features(ddf, column_id='metrics', column_sort="timestamp", column_value=col,
                                               default_fc_parameters=MinimalFCParameters(), disable_progressbar=True)
-        extracted_features.insert(loc=0,
-                                  column='metrics',
-                                  value=metrics_name + ":" + col)
+        index_name = metrics_name + ":" + col
+        cnt = ddf[ddf[col] > 0][col].count()
+        sum_div_cnt = 0 if cnt == 0 else extracted_features.iloc[0][0] / cnt
+        extracted_features.insert(loc=0, column='metrics', value=index_name)
         extracted_features.insert(loc=9, column='mean_mean', value=0)
         extracted_features.insert(loc=10, column='maximum_mean', value=0)
+        extracted_features.insert(loc=11, column='sum_div_cnt', value=sum_div_cnt)
+        extracted_features.insert(loc=12, column='mean_sum_div_cnt', value=0)
+        extracted_features.insert(loc=13, column='maximum_sum_div_cnt', value=0)
         new_cols = {x: y for x, y in zip(extracted_features.columns, table.columns)}
         # print(new_cols)
         extracted_features.rename(columns=new_cols, inplace=True)
         table = table.append(extracted_features, ignore_index=True)
     # extracted_features = extract_features(df, column_id='metrics', column_sort="timestamp")
+    # calculate some summary feature
     balance_summary = pd.Series([metrics_name, 0, 0, 0,
-                0, 0, 0, table['maximum'].max(), table['minimum'].min(), table['mean'].mean(), table['mean'].max()], index=feature_cols)
+        0, 0, 0,
+        table['maximum'].max(),
+        table['minimum'].min(),
+        table['mean'].mean(),
+        table['mean'].max(),
+        0,
+        table['sum_div_cnt'].mean(),
+        table['sum_div_cnt'].max()],
+        index= feature_cols)
     table = table.append(balance_summary, ignore_index=True)
     return table
 
@@ -56,7 +70,7 @@ if __name__ == "__main__":
         metrics = Path(file).stem
         data = load_csv(os.path.join(input_dir, file))
         print("extract {0} feature...".format(metrics))
-        features = extract_feature(data, metrics, False)
+        features = extract_feature(data, metrics, True)
         # write feature row by row
         if i == 0:
             features.to_csv(output_file, sep=',', index=False)
